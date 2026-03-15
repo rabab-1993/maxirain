69deb1299aef4edb652c923bbe0be4db6436bfa2
rabab-1993 <rababyousef1414@gmail.com>
2026-03-15 10:33:46 +0300

    update prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../app/generated/prisma/client"
+import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
@@ -6,9 +6,6 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
 });
-// const prisma = new PrismaClient({
-//     adapter,
-// });
 
 const prisma =
     globalForPrisma.prisma ||
